3e4193e1c140f3629e5386ae847a7f7699a1b8d0
Zahra Rostami <zororostami2001@gmail.com>
2023-02-03 22:12:59 +0330

    Start

--- main.cpp
+++ main.cpp
@@ -3,9 +3,6 @@
 using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Wow Oh My god!" << std::endl;
-    // what the hell? //
-    // yeeeyyy //
+
     return 0;
 }
