2b428731550c5f35c42118c8181f20dde3d93f6c
rezapanah2002 <rezapanah2002@gmail.com>
2023-02-05 11:27:12 +0330

    main

--- main.cpp
+++ main.cpp
@@ -4,5 +4,14 @@ using namespace std;
 
 int main() {
 
+    string n;
+
+    cout<< "1. status" << "\n" << "2. new" << "\n" << "3. index" << "\n" << "4. concat" << "\n" <<
+            "5. insert" << "\n" << "6. split" << "\n" << "7. delete" << "\n" << "8. report" <<
+            "\n" << "9. undo" << "\n" << ">>";
+
+    cin>> n;
+
+
     return 0;
 }
