c01c5ff6bac0384bac8e4387a3a076f753daa9a9
Zahra Rostami <zororostami2001@gmail.com>
2023-02-05 11:58:52 +0330

    .hup

--- TextEditor.h
+++ TextEditor.h
@@ -5,4 +5,18 @@
 #ifndef FINAL_TEXTEDITOR_H
 #define FINAL_TEXTEDITOR_H
 
+// max lenght of leaf
+const int ll = 2;
+
+class Rope
+{
+public:
+    Rope *left, *right, *parent;
+    char *str;
+    int lc;
+    void makerope(Rope *&node, Rope *par,char a[], int l, int r);
+    void prstr(Rope *r);
+
+};
+
 #endif //FINAL_TEXTEDITOR_H
--- main.cpp
+++ main.cpp
@@ -6,11 +6,11 @@ int main() {
 
     string n;
 
-    cout<< "1. status" << "\n" << "2. new" << "\n" << "3. index" << "\n" << "4. concat" << "\n" <<
+    cout << "1. status" << "\n" << "2. new" << "\n" << "3. index" << "\n" << "4. concat" << "\n" <<
             "5. insert" << "\n" << "6. split" << "\n" << "7. delete" << "\n" << "8. report" <<
-            "\n" << "9. undo" << "\n" << ">>";
+            "\n" << "9. undo" << "\n" << "Enter your requst: ";
 
-    cin>> n;
+    cin >> n;
 
 
     return 0;
